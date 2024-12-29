18826b3d543d0a8d9388c1b138dc593558be0d55
Artha210102 <arthanugraha7945@gmail.com>
2024-12-30 03:56:07 +0700

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,9 +1,9 @@
+import pickle
 import streamlit as st
 import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-import pickle
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 
